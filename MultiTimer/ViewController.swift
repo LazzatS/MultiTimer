9b876abbab35385055d5047cb10c1ee570df8db0
Lazzatios <lseiilova@nu.edu.kz>
2021-09-07 04:04:30 +0600

    feat: create view title

--- MultiTimer/ViewController.swift
+++ MultiTimer/ViewController.swift
@@ -11,12 +11,14 @@ class ViewController: UIViewController {
 
     let scrollView = UIScrollView()
     let contentView = UIView()
+    let viewTitle = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Мульти таймер"
-        view.backgroundColor = .cyan
+        view.backgroundColor = .white
         setupScrollView()
+        createViewTitle()
     }
     
     func setupScrollView(){
@@ -36,5 +38,30 @@ class ViewController: UIViewController {
         contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
     }
     
+    func createViewTitle() {
+        viewTitle.text = "Добавление таймеров"
+        viewTitle.textColor = .secondaryLabel
+        contentView.addSubview(viewTitle)
+        
+        let emptyView = UILabel()
+        emptyView.backgroundColor = .clear
+        contentView.addSubview(emptyView)
+        
+        viewTitle.translatesAutoresizingMaskIntoConstraints = false
+        viewTitle.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 30).isActive = true
+        viewTitle.widthAnchor.constraint(equalTo: contentView.widthAnchor).isActive = true
+        viewTitle.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        viewTitle.leftAnchor.constraint(equalTo: emptyView.rightAnchor, constant: 20).isActive = true
+        
+        addBottomBorder(for: viewTitle, with: .secondaryLabel, andWidth: 1)
+    }
+    
+    func addBottomBorder(for uiView: UIView, with color: UIColor?, andWidth borderWidth: CGFloat) {
+        let border = UIView()
+        border.backgroundColor = color
+        border.autoresizingMask = [.flexibleWidth, .flexibleTopMargin]
+        border.frame = CGRect(x: -20, y: uiView.frame.size.height - 15, width: uiView.frame.size.width + 20, height: borderWidth)
+        uiView.addSubview(border)
+    }
 }
 
