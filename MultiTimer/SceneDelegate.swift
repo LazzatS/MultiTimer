fa142be225f3f200f22710156798a56ad24a9365
Lazzatios <lseiilova@nu.edu.kz>
2021-09-06 20:15:33 +0600

    feat: create project without storyboard

---
+++ MultiTimer/SceneDelegate.swift
@@ -0,0 +1,40 @@
+//
+//  SceneDelegate.swift
+//  MultiTimer
+//
+//  Created by Lazzat Seiilova on 06.09.2021.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let rootVC = ViewController()
+        let nav = UINavigationController(rootViewController: rootVC)
+        window?.rootViewController = nav
+        window?.windowScene = windowScene
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+    }
+}
+
---
+++ MultiTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MultiTimer
+//
+//  Created by Lazzat Seiilova on 06.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Мульти таймер"
+        view.backgroundColor = .cyan
+    }
+    
+}
+
